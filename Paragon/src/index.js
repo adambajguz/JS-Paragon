9f25e97b3b94dff010ecb81dd4721ea9312e2298
Adam Bajguz <adam.bajguz@gmail.com>
2018-11-24 12:56:56 +0100

    Fixed bug with reordering buttons when deleting rows

--- Paragon/src/index.js
+++ Paragon/src/index.js
@@ -24,6 +24,7 @@ window.onload = function () {
 
         updateTableId();
         removeItemFromLocalStorage(row_id);
+        setMoveButtons();
     });
 
     $('#itemContainer').on('click', 'button.moveUp', function () {
